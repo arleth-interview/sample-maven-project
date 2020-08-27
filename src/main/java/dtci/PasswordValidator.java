4938b9babcbf97cb4fd1325d487bb1a462cb63f8
arleth-interview <pat.arleth@gmail.com>
2020-08-27 08:34:21 -0400

    initial commit

---
+++ src/main/java/dtci/PasswordValidator.java
@@ -0,0 +1,22 @@
+package dtci;
+
+import java.util.*;
+import java.net.*;
+import java.util.concurrent.*;
+import java.io.*;
+
+/**
+ * arleth-interview
+ * DTCI-interview1
+ */
+public class PasswordValidator {
+    public static void main(final String[] args) {
+        System.out.println("Welcome");
+        final PasswordValidator app = new PasswordValidator();
+
+    }
+
+    public String toString() {
+        return "why hello there -";
+    }
+}
---
+++ src/test/java/dtci/PasswordValidatorTest.java
@@ -0,0 +1,18 @@
+package dtci;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class PasswordValidatorTest {
+
+    /**
+     * Empty Test :-)
+     */
+    @Test
+    public void testPassword() {
+        Assert.assertTrue( true );
+    }
+}
